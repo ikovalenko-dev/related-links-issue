17bc309139408e95266899ece5811477b8af6edb
ikovalenko <ivan.kovalenko@koniglabs.ru>
2021-07-30 12:21:23 +0200

    code sample add

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # related-links-issue
 Repository to test related links change event issue
+
+# Build
+* ```npm run build``` - to build solution
+* ```npm run watch``` - to watch and build
+
+# Create extension package
+* ```npm run pack``` - .vsix package will be created and stored to ./bin folder
+* ```npm run pack:dev``` - package for local development
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log('test')
+import { IWorkItemFormService, IWorkItemNotificationListener, WorkItemTrackingServiceIds } from 'azure-devops-extension-api/WorkItemTracking';
+import * as SDK from 'azure-devops-extension-sdk';
+
+
+
+/**
+ * Initializes ADO extension view
+ * @param registerEvents - work item event listener factory
+ */
+export async function main(): Promise<void> {
+    await SDK.init();
+
+    const registerEvents = () => {
+        const listener: IWorkItemNotificationListener = {
+            onFieldChanged: args => {
+                const { changedFields } = args;
+                if (changedFields['System.RelatedLinks'] === undefined) {
+                    return;
+                }
+                SDK
+                    .getService<IWorkItemFormService>(WorkItemTrackingServiceIds.WorkItemFormService)
+                    .then(service => service.getWorkItemRelations())
+                    .then(relations => console.log(`current: ${changedFields['System.RelatedLinkCount']}`, `request result: ${relations.length}`))
+                    .catch(error => console.error(error))
+            },
+            onLoaded: args => console.log(args),
+            onRefreshed: args => console.log(args),
+            onReset: args => console.log(args),
+            onSaved: args => console.log(args),
+            onUnloaded: args => console.log(args)
+        }
+        return listener
+    };
+
+    SDK.register(SDK.getContributionId(), registerEvents);
+
+    await SDK.notifyLoadSucceeded();
+}
+
+main().catch(error => console.error(error))
